e384416183f03c813d943836dd879b9fe51c2165
Michael Checo <checomichael2@gmail.com>
2018-12-02 11:59:39 -0500

    added jwt layer

--- index.js
+++ index.js
@@ -1,23 +1,97 @@
 const express = require('express');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs')
 const db = require('./database/dbConfig.js');
+const jwt = require('jsonwebtoken');
 
 const server = express();
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/', (req, res) => {
-  res.send('Its Alive!');
+
+
+server.post('/api/register', (req, res) => {
+  // grab usernames and passwords from body
+  const creds = req.body;
+  // generate the hash from the user's password
+  const hash = bcrypt.hashSync(creds.password, 14) // rounds is 2^X
+  // override the user.password with the hash
+  creds.password = hash;
+ // save the user to the database
+db('users').insert(creds).then(
+  ids => {
+    res.status(201).json(ids);
+  }
+).catch(err => json({message: err}));
 });
 
+function protected(req, res, next) {
+  const token = req.headers.authorization;
+  if (token) {
+   jwt.verify(token, jwtSecret, (err, decodedToken) => {
+     if(err) {
+       res.status(401).json({message: 'invalid token'})
+     } else {
+       req.decodedToken = decodedToken;
+       next();
+     }
+   })
+  }  else {
+    res.status(401).json({message: 'no token'})
+  }
+}
+
+function checkRole(role) {
+  return function(req, res, next) {
+    if (req.decodedToken && req.decodedToken.roles.includes(role)) {
+      next()
+    } else {
+      res.status(403).json({message: 'Forbidden'})
+    }
+  }
+}
+
+const jwtSecret = 'nobody tosses a dwarf';
+
+function generateToken(user) {
+
+  const jwtPayload = {
+    ...user,
+    hello: 'FSW14',
+    roles: ['admin', 'other']
+  }
+
+
+  const jwtOptions = {
+    expiresIn: '5m'
+  }
+  return jwt.sign(jwtPayload, jwtSecret, jwtOptions)
+}
+
+server.post('/api/login', (req, res) => {
+  // grab usernames and passwords from body
+  const creds = req.body;
+
+  db('users').where({ username: creds.username }).first()
+  .then( user => {
+      if(user && bcrypt.compareSync(creds.password, user.password)) {
+        // passwords match and user exists by that username 
+        const token = generateToken(user)
+        res.status(200).json({message: 'Welcome!', token })
+      } else {
+        res.status(401).json({message: 'You shall not pass!'})
+      }}).catch(err => json(err))
+});
+
+
+
 // protect this route, only authenticated users should see it
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password') // added password to the select
     .then(users => {
-      res.json(users);
+      res.json({users});
     })
     .catch(err => res.send(err));
 });
